3290d6fc9da324a7429d603521e4d53dd9ec5ca2
MarkZither <mark.burton@zither-it.co.uk>
2022-11-25 23:06:56 +0100

    Setup routing to Santa component

--- src/SecretSanta.Angular/src/app/app-routing.module.ts
+++ src/SecretSanta.Angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SantaComponent } from './santa/santa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'santa', component: SantaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/SecretSanta.Angular/src/app/app.module.ts
+++ src/SecretSanta.Angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SantaComponent } from './santa/santa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SantaComponent
   ],
   imports: [
     BrowserModule,

e6e687ade3a18c8a0bb591330ddfa34c682dfa0b
MarkZither <mark.burton@zither-it.co.uk>
2022-11-27 21:40:47 +0100

    Add login component to redirect unauth requests to

--- src/SecretSanta.Angular/src/app/app-routing.module.ts
+++ src/SecretSanta.Angular/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from '@app/profile/profile.component';
 import { SantaComponent } from '@app/santa/santa.component';
 import { AuthGuard } from './core/guards/auth.guard';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'santa', component: SantaComponent },
@@ -10,8 +11,11 @@ const routes: Routes = [
       component: ProfileComponent, 
       canActivate: [AuthGuard], 
     data: {
-      authGuardRedirect: '/'
+      authGuardRedirect: '/login'
     } 
+  },
+  { path: 'login', 
+    component: LoginComponent
   },
 ];
 

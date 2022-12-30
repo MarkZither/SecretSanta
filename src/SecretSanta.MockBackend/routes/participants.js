28216081c87874663aaf84ed878081c6fdb13d43
MarkZither <mark.burton@zither-it.co.uk>
2022-12-30 22:12:16 +0100

    add restclient http for testing endpoints

--- src/SecretSanta.MockBackend/routes/participants.js
+++ src/SecretSanta.MockBackend/routes/participants.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET participant listing. */
 router.get('/', function(req, res, next) {
   res.setHeader('Content-Type', 'application/json');
   const participantsService = require('../services/participantsService');
   const participants = participantsService.getparticipantList();
   res.send(participants);
+});
+
+/* POST new participant. */
+router.post('/', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  const participantsService = require('../services/participantsService');
+  const participant = participantsService.addNewParticipant(req.body);
+  res.send(participant);
 });
 
 module.exports = router;

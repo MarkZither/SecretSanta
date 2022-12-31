dcd2e12622a23137ae0a58ea4ea04862efd8154f
MarkZither <mark.burton@zither-it.co.uk>
2022-12-31 21:38:40 +0100

    set env.js to use mock backend

--- src/SecretSanta.Angular/src/env.js
+++ src/SecretSanta.Angular/src/env.js
@@ -2,7 +2,7 @@
     window.__env = window.__env || {};
   
     // API url
-    window.__env.apiUrl = 'http://dev.your-api.com';
+    window.__env.apiUrl = 'http://localhost:5500/';
     
     // Whether or not to enable debug mode
     // Setting this to false will disable console output

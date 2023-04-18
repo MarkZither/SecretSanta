4189df3f3476512e772aef55469fbb0aa2f5d6f9
MarkZither <mark.burton@zither-it.co.uk>
2023-04-18 22:31:14 +0200

    implement the match generator controller

--- src/SecretSanta.MobileAppService/Controllers/MatchGeneratorController.cs
+++ src/SecretSanta.MobileAppService/Controllers/MatchGeneratorController.cs
@@ -1,9 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+using SecretSanta.Models;
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
 namespace SecretSanta.MobileAppService.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class MatchGeneratorController : ControllerBase {
+        // POST: api/Email
+        [HttpPost(Name = "GenerateMatchesUsingPost")]
+        public async Task<IActionResult> PostAsync([FromBody] MatchGeneratorModel model) {
+            var res = SecretSanta.Shared.SecretSantaGenerator.Generate(model.Participants, model.BannedPairs);
+            
+            return Ok();
+        }
+    }
+
+    public class MatchGeneratorModel {
+        public IList<ParticipantDTO> Participants { get; set; }
+        public List<KeyValuePair<ParticipantDTO, ParticipantDTO>> BannedPairs { get; set; }
     }
 }

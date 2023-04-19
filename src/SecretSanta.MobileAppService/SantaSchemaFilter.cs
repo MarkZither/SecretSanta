3fd04abe3365c568f2b7857046e5d3aec4bf5ed4
MarkZither <mark.burton@zither-it.co.uk>
2023-04-19 23:04:10 +0200

    add schemafilter to improve swagger examples

---
+++ src/SecretSanta.MobileAppService/SantaSchemaFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+
+using SecretSanta.MobileAppService.Controllers;
+
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace SecretSanta.MobileAppService {
+    public class SantaSchemaFilter : ISchemaFilter {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context) {
+            if (context.Type == typeof(MatchGeneratorModel)) {
+                schema.Example = new OpenApiObject() {
+                    ["firstName"] = new OpenApiString("John"),
+                    ["lastName"] = new OpenApiString("Doe"),
+                };
+            }
+        }
+    }
+}

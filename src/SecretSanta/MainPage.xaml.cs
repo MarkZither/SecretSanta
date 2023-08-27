026af46289f258cc51834545dfc51cf48de7268f
MarkZither <mark.burton@zither-it.co.uk>
2023-08-27 21:02:31 +0200

    setup maui mainpage

--- src/SecretSanta/MainPage.xaml.cs
+++ src/SecretSanta/MainPage.xaml.cs
@@ -1,20 +1,13 @@
-﻿namespace SecretSanta {
-    public partial class MainPage : ContentPage {
-        int count = 0;
+﻿using System;
 
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace SecretSanta {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage {
         public MainPage() {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e) {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 }

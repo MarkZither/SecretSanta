0a2d42e7308b2a42a4aecb865afce6caa2bc7f92
MarkZither <mark.burton@zither-it.co.uk>
2023-08-28 21:33:56 +0200

    add some content to items page

--- src/SecretSanta/Views/ItemsPage.xaml.cs
+++ src/SecretSanta/Views/ItemsPage.xaml.cs
@@ -21,6 +21,7 @@ namespace SecretSanta.Views
 	public partial class ItemsPage : ContentPage
 	{
         ItemsViewModel viewModel;
+        private int count = 0;
 
         public ItemsPage()
         {
@@ -46,6 +47,16 @@ namespace SecretSanta.Views
         async void AddItem_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+        }
+        private void OnCounterClicked(object sender, EventArgs e) {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
         }
 
         async void SantaItem_Clicked(object sender, EventArgs e)

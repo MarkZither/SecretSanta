fa121f8f0a994dc25a5f35564d413f37c8c1a35e
MarkZither <mark.burton@zither-it.co.uk>
2023-10-15 22:14:59 +0200

    fix warnings on required fields

--- src/SecretSanta.Blazor/Models/NewParticipant.cs
+++ src/SecretSanta.Blazor/Models/NewParticipant.cs
@@ -1,10 +1,17 @@
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace SecretSanta.Blazor {
     public class NewParticipant {
+        [SetsRequiredMembers]
+        public NewParticipant(string name, string email) {
+            Name = name;
+            Email = email;
+        }
+
         [Required]
-        public string Name { get; set; }
+        public required string Name { get; set; } = string.Empty;
         [Required]
-        public string Email { get; set; }
+        public required string Email { get; set; } = string.Empty;
     }
 }
